850f31447e6429f16cc38c137fa069d2530d7d30
Ali <ali005566alaa@gmail.com>
2025-10-06 21:30:50 +0300

    Set Up React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router'
+import {HomePage} from './Pages/HomePage'
+import {CheckoutPage} from './Pages/CheckoutPage'
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path= "checkout" element={<CheckoutPage />} />
+      </Routes>
     </>
   )
 }
